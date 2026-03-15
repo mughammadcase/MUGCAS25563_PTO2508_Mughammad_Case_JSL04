5ac38d8e619b1d936d6a6ddaa175cd350a405005
Mughammad <caseameen44@gmail.com>
2026-03-15 11:39:31 +0200

    Add DOM selectors for todo, doing, and done containers
    - Remove hardcoded tasks from HTML
    
    - Store container references using querySelectors

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 import { initialTasks } from "./initialData.js";
 
+/**
+ * Clones initialTasks data keeping original data intact
+ */
 const tasks = [...initialTasks];
 
-console.log("Initial Tasks:", tasks);
+const todoContainer = document.querySelector("#todo-container");
+const doingContainer = document.querySelector("#doing-container");
+const doneContainer = document.querySelector("#done-container");
+
+console.log(todoContainer, doingContainer, doneContainer); // Tests in browser console
