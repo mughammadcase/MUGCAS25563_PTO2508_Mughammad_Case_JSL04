f47daf6b0c105d587b7c005a0bb17dbc66121a95
Mughammad <caseameen44@gmail.com>
2026-03-15 12:10:45 +0200

    Add reusable function to generate task elements to the DOM
    - Add createTaskElement(task) function
    
    - Create a div for every task and show task titles
    
    - Remove empty task divs from HTML
    
    - Add task-div class to the created element
    
    - Test adding tasks to the DOM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { initialTasks } from "./initialData.js";
 
 /**
- * Clones initialTasks data keeping original data intact
+ * Clones initialTasks data for manipulating keeping original data intact
  */
 const tasks = [...initialTasks];
 
@@ -9,4 +9,22 @@ const todoContainer = document.querySelector("#todo-container");
 const doingContainer = document.querySelector("#doing-container");
 const doneContainer = document.querySelector("#done-container");
 
-console.log(todoContainer, doingContainer, doneContainer); // Tests in browser console
+/**
+ * Create DOM element for a task
+ * @param {Object} task - Task object contains id, title, description, ,status
+ * @return {HTMLElement} - Returns DOM element respresenting the task
+ */
+function createTaskElement(task) {
+  const taskDiv = document.createElement("div");
+
+  taskDiv.textContent = task.title;
+  taskDiv.classList.add("task-div");
+
+  return taskDiv;
+}
+
+// Testing if title of first task is correctly rendered in the DOM
+const firstTaskElement = createTaskElement(tasks[0]);
+const secondTaskElement = createTaskElement(tasks[1]);
+todoContainer.appendChild(firstTaskElement);
+todoContainer.appendChild(secondTaskElement);
